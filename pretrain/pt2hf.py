b7ce5a4914695078fc8e39c59e8f3caff4300fd9
GamjaPower <sst@eroun.ai>
2025-01-10 15:17:18 +0900

    feat: add bert-base-multilingual-uncased

--- pretrain/pt2hf.py
+++ pretrain/pt2hf.py
@@ -1,20 +1,19 @@
 from composer.models import HuggingFaceModel
 from transformers import AutoTokenizer
 
-# Composer 체크포인트에서 모델과 토크나이저 로드
-model, tokenizer = HuggingFaceModel.hf_from_composer_checkpoint(
-    checkpoint_path='checkpoints/latest-rank0.pt',
-    model_instantiation_class='transformers.AutoModelForSequenceClassification', 
-    model_config_kwargs={'num_labels': 2}  # 필요한 설정 추가
-)
-bert_tokenizer = AutoTokenizer('google-bert/bert-base-uncased')
+def convert():
+    # Composer 체크포인트에서 모델과 토크나이저 로드
+    model, tokenizer = HuggingFaceModel.hf_from_composer_checkpoint(
+        checkpoint_path='./work/ep2916-ba70000-rank0.pt',
+        model_instantiation_class='transformers.AutoModelForSequenceClassification', 
+        model_config_kwargs={'num_labels': 2}  # 필요한 설정 추가
+    )
+    bert_tokenizer = AutoTokenizer.from_pretrained('google-bert/bert-base-uncased')
 
-# Hugging Face 모델로 래핑
-hf_model = HuggingFaceModel(
-    model=model,
-    tokenizer=bert_tokenizer,
-    # metrics=metrics,
-    use_logits=True
-)
+    model.save_pretrained('./work/hf_model')
+    bert_tokenizer.save_pretrained('./work/hf_model')
+    
 
-hf_model.save_pretrained('hf_model')
+
+if __name__ == "__main__":
+    convert()
--- pretrain/test.py
+++ pretrain/test.py
@@ -7,15 +7,16 @@ def main():
     pipe = pipeline(
         "fill-mask",
         # model="answerdotai/ModernBERT-base",
-        model="google-bert/bert-base-multilingual-uncased",        
+        # model="google-bert/bert-base-multilingual-uncased",
         # model="microsoft/mdeberta-v3-base",
         # model="sigridjineth/ModernBERT-Korean-ColBERT-preview-v1",
-        
+        # model="kisti/korscideberta",
+        model="./work/hf_model",
         torch_dtype=torch.bfloat16,
     )
 
-    # input_text = "He walked to the [MASK]."
-    input_text = "다음주 월요일에 온라인 미팅 [MASK]."
+    input_text = "He walked to the [MASK]."
+    # input_text = "다음주 월요일에 온라인 미팅 [MASK]."
     results = pipe(input_text)
     pprint(results)
 
